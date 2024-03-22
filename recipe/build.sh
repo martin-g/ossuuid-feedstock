f48e9cf1db34e72a859581b9f807eecc81b866aa
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-03-22 11:17:02 +0200

    Fix lint errors
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # update config.guess and config.sub to newer versions which support more CPU architectures
 rm -f config.sub config.guess
 wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD" -O config.guess
