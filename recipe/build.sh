fcc775918ee0dcd82426b830a85a7c53aad37321
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-03-22 11:00:37 +0200

    Use newer config.(guess|sub) to support more CPU architectures (e.g. aarch64)
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# update config.guess and config.sub to newer versions which support more CPU architectures
+rm -f config.sub config.guess
+wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD" -O config.guess
+wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD" -O config.sub
+
 ./configure  --prefix=$PREFIX
 make -j
 make install
