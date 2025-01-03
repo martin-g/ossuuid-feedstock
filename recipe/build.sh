3fd1dc2cec56babaadbf63c19a741c1b070e3104
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-03 13:27:31 +0200

    Move `wget` to `build` reqs
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -x
+set -xe
 
 # update config.guess and config.sub to newer versions which support more CPU architectures
 rm -f config.sub config.guess
 wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD" -O config.guess
 wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD" -O config.sub
 
-./configure  --prefix=$PREFIX
-make -j
+./configure --prefix=$PREFIX
+make -j "${CPU_COUNT}"
 make install
 make check
